0569018fcc5c250b7d63776f71af074519cef65d
Yogendra <bkyogendra246@gmail.com>
2026-08-04 21:32:23 +0545

    Add database connection utility using mongoose

---
+++ server/utils/db.js
@@ -0,0 +1,15 @@
+import mongoose from 'mongoose';
+
+const connectDB  = async() =>{
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {
+            dbname: "Ecommerce2026",
+        });
+        console.log("MongoDB Connected")
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+export default connectDB;
